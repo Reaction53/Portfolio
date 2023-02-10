c4e028c45faee703e843877b1df0de51ee691ea3
Douglas Reis <douglasreais36@gmail.com>
2023-02-10 01:03:05 -0300

    Some media queries

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -5,27 +5,27 @@ import { motion } from 'framer-motion';
 function Bio () {
 
   return (
-    <div className="grid grid-cols-2 w-[100vw] h-[60vw]">
+    <div className="grid grid-cols-2 w-[100vw] h-[100vw] lg:flex flex-wrap">
       <motion.div
           initial={{opacity:0 , scale: 0}}
           whileInView={{ opacity: 1 , scale: 1}}
           transition={{ease: [0, 0.71, 0.2, 1.01]}}>
 
         <h1 className="text-center text-gray-300 text-4xl ">Sites Personalizados</h1>
-        <p className="text-center text-gray-300 text-xl p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. At rerum minima ea aut unde, nulla doloremque suscipit quibusdam voluptatum deleniti.</p>   
+        <p className="text-center text-gray-300 text-xl p-4 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. At rerum minima ea aut unde, nulla doloremque suscipit quibusdam voluptatum deleniti.</p>   
       </motion.div>
       <motion.div className="grid justify-items-center"
           initial={{opacity:0 , scale: 0}}
           whileInView={{ opacity: 1 , scale: 1}}
           transition={{ease: [0, 0.71, 0.2, 1.01]}}>
-        <img alt="Site" src={Site} className="w-[80%] h-[80%]"></img>
+        <img alt="Site" src={Site} className="w-[50%] h-[50%] lg:hidden"></img>
       </motion.div>
 
       <motion.div className="grid justify-items-center"
       initial={{opacity:0 , scale: 0}}
       whileInView={{ opacity: 1 , scale: 1}}
       transition={{ease: [0, 0.71, 0.2, 1.01]}}>
-          <img alt="Hora" src={Hora} className="w-[80%] h-[80%]"></img>
+          <img alt="Hora" src={Hora} className="w-[50%] h-[50%] lg:hidden"></img>
         </motion.div>
         <motion.div
         initial={{opacity:0 , scale: 0}}
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -35,7 +35,7 @@ function Content (){
         
 
 
-        <motion.div className="grid grid-cols-2 gap-4 place-items-center h-screen"
+        <motion.div className="grid grid-cols-2 gap-4 place-items-center h-screen lg:grid-cols-1"
 
          initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -44,7 +44,7 @@ function Content (){
             delay: 1.6,
             ease: [0, 0.71, 0.2, 1.01]
           }}>
-            <motion.div animate={{height: isOpen ? 200 : 90, width: isOpen ? 400 : 200}} onClick={handleClick} className="bg-gradient-to-r from-[#0D1212] to-[#060808] text-gray-300 rounded shadow-lg shadow-black w-[25%] h-[30%] mr-[-35%] cursor-pointer">
+            <motion.div animate={{height: isOpen ? 200 : 90, width: isOpen ? 400 : 200}} onClick={handleClick} className="bg-gradient-to-r from-[#0D1212] to-[#060808] text-gray-300 rounded shadow-lg shadow-black w-[25%] h-[30%] mr-[-35%] lg:mr-[2%] cursor-pointer">
              <h1 className="flex"><DiReact className="h-8 w-8" />React</h1>
                  {isOpen && <motion.p 
                         transition={{ delay: 0.3, type: "spring"}}
@@ -59,7 +59,7 @@ function Content (){
                         }
             </motion.div>
 
-            <motion.div animate={{height: isOpen2 ? 200 : 90, width: isOpen2 ? 500 : 400}} onClick={handleClick2} className="bg-gradient-to-r from-[#0D1212] to-[#060808] text-gray-300 rounded shadow-lg shadow-black w-[25%] h-[30%] ml-[-25%] cursor-pointer">
+            <motion.div animate={{height: isOpen2 ? 200 : 90, width: isOpen2 ? 500 : 400}} onClick={handleClick2} className="bg-gradient-to-r from-[#0D1212] to-[#060808] text-gray-300 rounded shadow-lg shadow-black w-[25%] h-[30%] ml-[-25%] lg:mb-[20%] cursor-pointer">
             <h1 className="flex"><SiFramer className="h-6 w-6"/>Frame Motion</h1>
                 {isOpen2 && <motion.p 
                         transition={{ delay: 0.3, type: "spring"}}
@@ -69,7 +69,7 @@ function Content (){
                          Lorem ipsum dolor sit, amet consectetur adipisicing elit. </motion.p> }
             </motion.div>
 
-            <motion.div animate={{height: isOpen3 ? 200 : 90, width: isOpen3 ? 500 : 400}} onClick={handleClick3} className="bg-gradient-to-r from-[#0D1212] to-[#060808] text-gray-300 rounded shadow-lg shadow-black w-[25%] h-[30%] mt-[-35%] mr-[-35%] cursor-pointer">
+            <motion.div animate={{height: isOpen3 ? 200 : 90, width: isOpen3 ? 500 : 400}} onClick={handleClick3} className="bg-gradient-to-r from-[#0D1212] to-[#060808] text-gray-300 rounded shadow-lg shadow-black w-[25%] h-[30%] mt-[-35%] mr-[-35%] lg:ml-[-15%] cursor-pointer">
             <h1 className="flex"><AiFillHtml5 className="h-6 w-6"/>HTML5 e CSS3 <DiCss3 className="h-6 w-6"/></h1>
                 {isOpen3 && <motion.p 
                         transition={{ delay: 0.3, type: "spring"}}
@@ -79,7 +79,7 @@ function Content (){
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. </motion.p> }
             </motion.div>
             
-            <motion.div animate={{height: isOpen4 ? 200 : 90, width: isOpen4 ? 400 : 200}} onClick={handleClick4}className="bg-gradient-to-r from-[#0D1212] to-[#060808] text-gray-300 rounded shadow-lg shadow-black w-[75%] h-[30%] mt-[-35%] ml-[-25%] cursor-pointer">
+            <motion.div animate={{height: isOpen4 ? 200 : 90, width: isOpen4 ? 400 : 200}} onClick={handleClick4}className="bg-gradient-to-r from-[#0D1212] to-[#060808] text-gray-300 rounded shadow-lg shadow-black w-[75%] h-[30%] mt-[-35%] ml-[-25%] lg:ml-[-2%] lg:mt-[-15%] cursor-pointer">
             <h1 className="flex"><DiJavascript1 className="h-6 w-6"/>JavaScript</h1>
                {isOpen4 && <motion.p
                         transition={{ delay: 0.3, type: "spring"}}
