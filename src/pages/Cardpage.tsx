b0a183b7cd102d2955f5b1120e749aaac4800e03
Douglas Reis <douglasreais36@gmail.com>
2023-02-25 03:07:47 -0300

    responsive changes

--- src/pages/Cardpage.tsx
+++ src/pages/Cardpage.tsx
@@ -21,7 +21,7 @@ const Cardpage = () => {
         animate={{ opacity: 1, top: "3rem" }}
         className='text-center text-gray-300 text-4xl p-10 lg:hidden'> Clique e arraste!</motion.h1>
         </div>
-        <div style={{ perspective: 2000}} className='flex items-center ml-[-32%] lg:mt-[25%] lg:ml-[0]'>
+        <div style={{ perspective: 2000}} className='flex items-center ml-[-32%] lg:mt-[25%] lg:ml-[20%]'>
           <motion.div
             style={{x, y, rotateX, rotateY, z: 100}}
             drag
@@ -31,7 +31,7 @@ const Cardpage = () => {
 
           className='w-[400px] min-h-[500px] bg-gradient-to-r from-[#0D1212] to-[#060808] text-gray-300 shadow-lg shadow-black rounded-lg px-[40px] py-[25px] cursor-grab relative lg:w-[80%] lg:h-[30%]'>{/*Card*/}
           
-            <h1 className='text-5xl mb-6 font-extrabold lg:p-30 lg:text-center lg:text-2xl'>Cards Animados!</h1>{/*Titulo*/}
+            <h1 className='text-5xl mb-6 font-extrabold lg:p-30 lg:text-center lg:text-xl'>Cards Animados!</h1>{/*Titulo*/}
 
             <p className='max-w-[300px] mb-6 text-gray-300 lg:p-30'>
               Animações de cards 3D, não muito utilizadas, porém, com uso da criatividade pode se tornar algo realmente confortável de se olhar 
@@ -45,7 +45,7 @@ const Cardpage = () => {
             <motion.div 
               style={{ x, y, rotateX, rotateY, z: 10000}}
 
-            className='absolute top-[35%] lg:mt-[20%]'>{/*Image*/}
+            className='absolute top-[35%] lg:mt-[10%]'>{/*Image*/}
               <img alt='women' src={Women} draggable='false'></img>
             </motion.div>{/*Image*/}
 
